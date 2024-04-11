a29c9d0f3af46de50edca9f4df8d74c8cd7ea04b
Nathaniel Frei <freinathaniel@yahoo.com>
2024-04-11 18:09:22 -0400

    Made fixes to my site.js to be able to find case in-sensitive palindromes while getting rid of special characters and whitespaces.

--- js/site.js
+++ js/site.js
@@ -1,14 +1,55 @@
+// get the string from the user
+function getValues() {
+    let inputString = document.getElementById('inputString').value;
+     let replaceCharacters = '';
+     let inspectedString = inspectCharacters(inputString, replaceCharacters);
+    
+    let stringResult = checkForPalindrome(inspectedString);
 
-// Display the Message the user entered.
-function displayMessage() {
-    let inputBox = document.getElementById('message');
-    let message = inputBox.value;
+    displayResults(stringResult, inputString);
+}
 
-    Swal.fire({
-        title: "App Name",
-        text: message,
-        icon: "success",
+// Inspects and replaces any special characters or spaces in inputString
+function inspectCharacters(inputString, replaceCharacters) {
+    return inputString.replaceAll(/[^a-zA-Z0-9]/gi, replaceCharacters);
+}
+// Check whether a string is a Palindrome or not
+function checkForPalindrome(inspectedString) {
+    let result = '';
+    let inspect = '';
+
+    for(let index = inspectedString.length - 1; index >= 0; index-- ) {
+      inspect += inspectedString[index];
+    }
+
+    if (inspect == inspectedString) {
+        result = "isPalindrome";
+    } else {
+      result = 'notPalindrome';
+    }
+    
+    return result;
+
+}
+
+// Display the results from checkForPalindrome to the user
+function displayResults(stringResult, inputString) {
+    
+    if(stringResult == 'isPalindrome') {
+      // display a corecct message
+      Swal.fire({
+        icon: 'success',
+        title: 'Nice!',
+        text: `Yes ${inputString} is a Palindrome`,
         backdrop: false
-      });
-
+    });
+    } else if(stringResult == 'notPalindrome') {
+      // display a corecct message
+      Swal.fire({
+        icon: 'error',
+        title: 'Sorry!',
+        text: `No, unfortunately ${inputString} is not a Palindrome`,
+        backdrop: false
+    });
+    }
 }
